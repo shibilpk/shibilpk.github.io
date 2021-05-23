d23f3d2c8754bfd4bd908b542453f27a8aa96242
Shibil pk <shibil.pk.786@gmail.com>
2021-05-23 16:27:27 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function resize() {
         let card_ids = [
             "#skill section.wrapper section.container div.card",
             "#github section.wrapper section.container ul li",
+            "#projects ul li",
         ];
         let initial_width = "30px";
         let marge_width = 0;
@@ -93,10 +94,15 @@ $(document).ready(function () {
         callback: function (box) {
             // the callback is fired every time an animation is started
             // the argument that is passed in is the DOM node being animated
+            setTimeout(function () {
+                // 
+            }, 1500);
+
         },
         scrollContainer: null, // optional scroll container selector, otherwise use window,
         resetAnimation: true, // reset animation on end (default is true)
     });
+    
     
 
     $(".page-transition").toggleClass("slide");
@@ -123,6 +129,30 @@ $(document).ready(function () {
 
 });
 
+// check element at view point===========
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+$(window).scroll(function () {
+    if ($('#spotlight .wrapper section.container div.description h1 ').isInViewport()) {
+        //  Use #spotlight .wrapper section.container div.description h1 instead of this
+        $('#spotlight .wrapper section.container div.description h1').addClass('make-move');
+        
+    } else {
+        //  Remove class
+        $('#spotlight .wrapper section.container div.description h1').removeClass('make-move');
+        
+    }
+});
+//======================
+
+
 $(window).resize(function () {
     resize();
 });
@@ -130,3 +160,143 @@ $(window).resize(function () {
 $(window).on("load", function () {
     resize();
 });
+
+/* Go to top button  */
+
+$(document).on("click", "a.backtotop", function (a) {
+    $("html, body").animate({ scrollTop: 0 }, 800), a.preventDefault();
+});
+$(document).on("scroll", function () {
+    200 < $(window).scrollTop() ? $(".backtotop").addClass("active") : $(".backtotop").removeClass("active");
+});
+
+/* Go to top button end */
+
+(function ($) {
+
+    $.fn.directionalHover = function(options) {
+        // Extend default plugin options
+        var opts = $.extend({}, $.fn.directionalHover.defaults, options);
+
+        // Create bit flags
+        var FLAG_T = 1, // top
+            FLAG_R = 2, // right
+            FLAG_B = 4, // bottom
+            FLAG_L = 8; // left
+
+        // Create bit masks
+        var tlMask = FLAG_T | FLAG_L,
+            trMask = FLAG_T | FLAG_R,
+            blMask = FLAG_B | FLAG_L,
+            brMask = FLAG_B | FLAG_R;
+
+        function slideOverlay(overlay, direction, px, py, ew, eh, ex, ey) {
+            var cornerFlags = 0; // top|right|bottom|left
+
+            if (py - ey <= eh / 2) cornerFlags ^= FLAG_T;
+            if (px - ex >= ew / 2) cornerFlags ^= FLAG_R;
+            if (py - ey >  eh / 2) cornerFlags ^= FLAG_B;
+            if (px - ex <  ew / 2) cornerFlags ^= FLAG_L;
+
+            findSide(cornerFlags, overlay, direction, px-ex, py-ey, ew/2, eh/2);
+        }
+
+        function findSide(flags, overlay, direction, x, y, w, h) {
+            if (testMask(flags, tlMask)) {
+                testTopLeftToBottomRight(x, y, w, h) ? setOverlayPosition(overlay, direction, 0, -w*2) : setOverlayPosition(overlay, direction, -h*2, 0);
+            }
+            else if (testMask(flags, trMask)) {
+                testBottomRightToTopLeft(x, y, w, h) ? setOverlayPosition(overlay, direction, -h*2, 0) : setOverlayPosition(overlay, direction, 0, w*2);
+            }
+            else if (testMask(flags, blMask)) {
+                testBottomRightToTopLeft(x, y, w, h) ? setOverlayPosition(overlay, direction, 0, -w*2) : setOverlayPosition(overlay, direction, h*2, 0);
+            }
+            else if (testMask(flags, brMask)) {
+                testTopLeftToBottomRight(x, y, w, h) ? setOverlayPosition(overlay, direction, h*2, 0) : setOverlayPosition(overlay, direction, 0, w*2);
+            }
+        }
+
+        function testMask(flags, mask) {
+            return (flags & mask) === mask;
+        }
+
+        function testTopLeftToBottomRight(x, y, w, h) {
+            return (h * x - w * y) < 0;
+        }
+
+        function testBottomRightToTopLeft(x, y, w, h) {
+            return (w * (y-h) + h * x - w * h) < 0;
+        }
+
+        function setOverlayPosition(overlay, direction, top, left) {
+            if (direction === 'in') {
+                overlay.animate({
+                    top: top,
+                    left: left
+                }, 0, function() {
+                    overlay.stop().animate({
+                        top: 0,
+                        left: 0
+                    }, opts.speed, opts.easing);
+                });
+            }
+            else if (direction === 'out') {
+                overlay.animate({
+                    top: 0,
+                    left: 0
+                }, 0, function() {
+                    overlay.stop().animate({
+                        top: top,
+                        left: left
+                    }, opts.speed, opts.easing);
+                });
+            }
+        }
+
+        this.css({
+            position: 'relative',
+            overflow: 'hidden'
+        });
+
+        this.find('.' + opts.overlay).css({
+            position: 'absolute',
+            top: '-100%'
+        });
+
+        return this.each(function() {
+            var container = $(this);
+
+            container.hover(function(e) {
+                slideOverlay(
+                    container.find('.' + opts.overlay),
+                    'in',
+                    e.pageX,
+                    e.pageY,
+                    container.width(),
+                    container.height(),
+                    Math.floor(container.offset().left),
+                    container.offset().top
+                );
+            }, function(e) {
+                slideOverlay(
+                    container.find('.' + opts.overlay),
+                    'out',
+                    e.pageX,
+                    e.pageY,
+                    container.width(),
+                    container.height(),
+                    Math.floor(container.offset().left),
+                    container.offset().top
+                );
+            });
+        });
+    };
+
+    // Plugin default options
+    $.fn.directionalHover.defaults = {
+        overlay: "dh-overlay",
+        easing: "swing",
+        speed: 400
+    };
+
+}(jQuery));
